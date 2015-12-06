642ced46324ee87116289ed7323654cf9df4bd6a
Kelvin Louis <me@kelvinlouis.com>
2015-12-06 21:55:33 +0100

    chp: changed light signal assignment (thx 2 udem)

--- chp/execises/6_1/script_6-1_17.asm
+++ chp/execises/6_1/script_6-1_17.asm
@@ -3,19 +3,34 @@
 ;----------------------------------------
 
 ;   Exercise Page 17
+Rest:
+    MOV AL,00
+    OUT 00
 RG:
     MOV AL,82
     OUT 00
-RYY:
-    MOV AL,C4
+RY:
+    MOV AL,84
     OUT 00
-GY:
+RR:
+    MOV AL,88
+    OUT 00
+RYR:
+    MOV AL,C8
+    OUT 00
+GR:
     MOV AL,28
     OUT 00
-YRY:
-    MOV AL,4C
+YR:
+    MOV AL,48
     OUT 00    
-    
-    JMP RG    
+RR2:
+    MOV AL,88
+    OUT 00
+RRY:
+    MOV AL,8C
+    OUT 00
+
+    JMP RG
 
 END
