d6cf63c59a0b280f92c51a6c6cfdc6a49307e5a2
Kelvin Louis <me@kelvinlouis.com>
2016-01-11 17:36:24 +0100

    fprog: solution to the final exams

--- fprog/exercises/15_finalExam/finalExam.hs
+++ fprog/exercises/15_finalExam/finalExam.hs
@@ -1,5 +1,7 @@
 import Data.Char
 
+-- 2
+
 -- WRONG ANSWER :(
 cleanString1 :: String -> String
 cleanString1 [] = []
@@ -19,3 +21,19 @@ cleanString3 xs = map toLower $ filter (\x -> not $ x == ' ') xs
 cleanString4 xs = foldr con "" xs
                 where con l acc = if isNotSpace l then ((toLower l):acc) else acc
                       isNotSpace c = not $ c == ' '
+
+--- 3
+data FS = File String Int 
+        | Folder String [FS]
+
+fprog = (Folder "fprog" [(Folder "x" [(File "word.doc" 4), (File "spreadsheet.xls" 6)]),(File "ls.txt" 4)])
+fprog2 = (Folder "fprog" [(Folder "x" [(File "spreadsheet.xls" 6),(File "word.doc" 4)]),(File "ls.txt" 4)])
+
+size :: FS -> Int
+size (File _ s) = s
+size (Folder _ xs) = sum $ map size xs
+
+instance Eq FS where
+  (File n _) == (File m _) = n == m
+  (File _ _) == (Folder _ _) = False
+  (Folder n xs) == (Folder m ys) = n == m && (foldr (\x acc -> if x == False then False else acc) True (zipWith (==) xs ys))
