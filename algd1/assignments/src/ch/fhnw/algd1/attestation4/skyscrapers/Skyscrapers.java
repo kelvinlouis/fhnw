a9c4fe3c0235632b099b92ed881808f6423a5d8a
Kelvin Louis <me@kelvinlouis.com>
2016-06-13 09:50:00 +0200

    algd1: wip

--- algd1/assignments/src/ch/fhnw/algd1/attestation4/skyscrapers/Skyscrapers.java
+++ algd1/assignments/src/ch/fhnw/algd1/attestation4/skyscrapers/Skyscrapers.java
@@ -6,42 +6,76 @@ import java.util.Arrays;
  * Created by Kelvin on 12-Jun-16.
  */
 public class Skyscrapers {
-
+    static int[][] solution = new int[4][4];
     static int[][] m;
 
-    private static boolean solve(int[] v, int row) {
-        int lb = m[row][0];
-        int rb = m[row][m.length-1];
-        int lc = 1;
-        int rc = 1;
+    private static boolean solve(int[] v) {
+        for (int x = 0; x < 4; x++) {
+            int lc = 1;
+            int rc = 1;
 
-        for (int i = 1; i < 4; i++) {
-            int rev = (4 - 1) - i;
+            int lb = m[x+1][0];
+            int rb = m[x+1][m.length-1];
+            int tb = m[0][x+1];
+            int bb = m[m.length-1][x+1];
 
-            if (v[0] < v[i]) {
-                lc++;
+            int lmax = v[0];
+            int rmax = v[3];
+
+            for (int i = 1; i < 4; i++) {
+                int rev = (4 - 1) - i;
+
+                if (lmax < v[i]) {
+                    lmax = v[i];
+                    lc++;
+                }
+
+                if (rmax < v[rev]) {
+                    rmax = v[rev];
+                    rc++;
+                }
             }
 
-            if (v[3] < v[rev]) {
-                rc++;
+            if (lb == lc && rb == rc) {
+                System.out.println("Y(" + (x+1)  + "): " + Arrays.toString(v));
+                pushRowSolution(v, x);
+            }
+
+            if (tb == lc && bb == rc) {
+                System.out.println("X(" + (x+1) + "): " + Arrays.toString(v));
+                pushColumnSolution(v, x);
             }
         }
 
-        return lc == lb && rc == rb;
+        return false;
     }
 
-    private static void fill(int[] v, int x, int y) {
-        if (x == v.length + 1) {
+    private static void pushRowSolution(int[] v, int y) {
+        for (int i = 0; i< v.length; i++) {
+            if (solution[y][i] == 0 || solution[i][i] == v[i])
+                solution[y][i] = v[i];
+        }
+    }
+
+    private static void pushColumnSolution(int[] v, int x) {
+        for (int i = 0; i< v.length; i++) {
+            if (solution[i][x] == 0 || solution[i][x] == v[i])
+                solution[i][x] = v[i];
+        }
+    }
+
+    private static void fill(int[] v, int d) {
+        if (d == v.length + 1) {
             // Check if boundaries of row are correct
-            if (solve(v, y)) {
-                System.out.println(y + "(Y): " + Arrays.toString(v));
+            if (solve(v)) {
+                System.out.println("SOLVED:" + Arrays.toString(v));
             }
             return;
         }
         for (int i = 0; i < v.length; i++) {
             if (v[i] == 0) {
-                v[i] = x;
-                fill(v, x + 1, y);
+                v[i] = d;
+                fill(v, d + 1);
                 v[i] = 0;
             }
         }
@@ -49,14 +83,12 @@ public class Skyscrapers {
 
     public static void main(String[] args) {
         m = createMatrix();
-        int n = m.length-2;
+        //int n = m.length-2;
 
         // Creates all rows
-        for (int y = 1; y <= n; y++) {
-            fill(new int[4], 1, y);
-        }
+        fill(new int[4], 1);
 
-        //printMatrix(m);
+        printMatrix(solution);
     }
 
     public static int[][] createMatrix() {
